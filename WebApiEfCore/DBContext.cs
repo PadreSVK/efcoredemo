7b1cd60a21ba4bce77b3939099d6a191d47d4c10
Patrik Švikruha <patrik.svikruha@gmail.com>
2023-11-15 15:33:07 +0100

    init EF Core sample

---
+++ WebApiEfCore/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiEfCore
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+
+
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+    }
+
+    public class User : IEntity
+    {
+        public Guid Id { get; set; }
+        public int Age { get; set; }
+        public required string Name { get; set; }
+    }
+}
